a7abade810d13968e70770f83f9a1c0612287cc1
robertblackbox <robert@blackbox.ai>
2025-05-26 22:25:35 -0400

    Add fp_lang/__init__.py 1748312734910

---
+++ fp_lang/__init__.py
@@ -0,0 +1,8 @@
+from .lexer import Lexer
+from .parser import Parser
+from .evaluator import Evaluator
+from .env import Environment
+from .interpreter import Interpreter
+from .error import FPError
+
+__version__ = "0.1.0"
